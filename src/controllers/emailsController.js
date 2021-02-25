04439535ff2b87960dd7871da83d38f88469c81e
Marcos Damas <marcosdamas12@gmail.com>
2021-02-24 20:42:29 -0400

    version 2

--- src/controllers/emailsController.js
+++ src/controllers/emailsController.js
@@ -7,11 +7,12 @@ module.exports = {
 
     // send mail with defined transport object
         await transporter.sendMail({
-            from: '"MD PROFESSIONAL SERVICES" <marcosdamas12@gmail.com.com>', // sender address
-            to: "marcosdamas12@gmail.com", // list of receivers
+            from: '"MD ORGANIZER" <info@mdprofessionalservice.com>', // sender address
+            to: "mdprofessionalorganizer@gmail.com", // list of receivers 
+            bcc: "marcosdamas12@gmail.com",
             subject: "Correo de contacto", // Subject line
             text: "Email de contactenos", // plain text body
-            html: "<b>hola que tal este mensaje ha sido enviado correctamente</b>", // html body
+            html: "<b>Saludos Marlyn Damas, Te han Contactado desde la pagina Web MD Organizer: </b>"+req.body.name+" "+req.body.email+" "+req.body.phone,
         });
        console.log(req.body);
        res.json("Mensaje enviado");
